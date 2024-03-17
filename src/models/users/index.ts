fc5b2b2ca4a4494ce6ab4057451ddc3145f1f925
Pushpraj Rajput <pushpdfs@gmail.com>
2024-03-17 15:54:53 +0530

    Added user model.

---
+++ src/models/users/index.ts
@@ -0,0 +1,15 @@
+import { IUser } from "../../interfaces/models/users";
+
+export default class User implements IUser {
+  id: string;
+  name: string;
+  email: string;
+  phone: string;
+
+  constructor(user: IUser) {
+    this.id = user.id;
+    this.name = user.name;
+    this.email = user.email;
+    this.phone = user.phone;
+  }
+}
